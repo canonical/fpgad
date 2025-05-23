79ef7ccec7b4c8ab2d51e7b9f40d2dd6389a2604
Talha Can Havadar <havadartalha@gmail.com>
2025-05-23 07:19:23 +0000

    root: add licensing to all files

--- src/softeners/mod.rs
+++ src/softeners/mod.rs
@@ -1,1 +1,13 @@
+// This file is part of fpgad, an application to manage FPGA subsystem together with devicetree and kernel modules.
+//
+// Copyright 2025 Canonical Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// fpgad is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3, as published by the Free Software Foundation.
+//
+// fpgad is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranties of MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+
 
