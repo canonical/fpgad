407d4c4e042d98606cfbefcd611fb614e62a1a44
Talha Can Havadar <talha.can.havadar@canonical.com>
2025-06-23 12:09:23 +0000

    cli: enable status sub command
    Enabled cli application and status subcommand using clap.
    This makes `fpga [--handle=<device_handle>] status` syntax available.
    
    Signed-off-by: Talha Can Havadar <talha.can.havadar@canonical.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand, arg, command};
+use log::debug;
+
+#[derive(Parser, Debug)]
+#[command(name = "fpga")]
+#[command(bin_name = "fpga")]
+struct Cli {
+    #[arg(
+        long = "handle",
+        help = r#"fpga device handle to be used for operations.
+Default value for this option is calculated in runtime and application
+picks first available fpga in the system (under /sys/class/fpga_manager).
+        "#
+    )]
+    handle: Option<String>,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Status,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    let cli = Cli::parse();
+    debug!("parsed cli command with {cli:?}");
+    match cli.command {
+        Commands::Status => {
+            todo!()
+        }
+    }
 }
