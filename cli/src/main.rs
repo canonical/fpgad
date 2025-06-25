ba3bde9b742aa03a635975d07d8c13d537ac1dae
Talha Can Havadar <talha.can.havadar@canonical.com>
2025-06-25 08:56:17 +0200

    cli: enable status sub command (#21)
    * cli: enable status sub command
    
    Enabled cli application and status subcommand using clap.
    This makes `fpga [--handle=<device_handle>] status` syntax available.
    
    * cli: convert help text into doc string for status command
    
    No need to write help texts in ugly way, doc strings
    works for both documentation and cli `--help` string
    
    ---------
    
    Signed-off-by: Talha Can Havadar <talha.can.havadar@canonical.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand, arg, command};
+use log::debug;
+
+#[derive(Parser, Debug)]
+#[command(name = "fpga")]
+#[command(bin_name = "fpga")]
+struct Cli {
+    /// fpga device `HANDLE` to be used for the operations.
+    /// Default value for this option is calculated in runtime and the application
+    /// picks the first available fpga in the system (under /sys/class/fpga_manager)
+    #[arg(long = "handle")]
+    handle: Option<String>,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Get the status information for the given device handle
+    Status,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    let cli = Cli::parse();
+    debug!("parsed cli command with {cli:?}");
+    match cli.command {
+        Commands::Status => {
+            todo!()
+        }
+    }
 }
