e0ed89e8ad874a44cb2ccb73940070cf755be71f
Artie Poole <stuart.poole@canonical.com>
2025-07-03 13:53:53 +0100

    cli: create proxy files to match the dbus interfaces
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

---
+++ cli/src/proxies/mod.rs
@@ -0,0 +1,3 @@
+pub mod configure_proxy;
+pub mod control_proxy;
+pub mod status_proxy;
---
+++ cli/src/proxies/status_proxy.rs
@@ -0,0 +1,15 @@
+use zbus::{Result, proxy};
+#[proxy(
+    default_service = "com.canonical.fpgad",
+    interface = "com.canonical.fpgad.status",
+    default_path = "/com/canonical/fpgad/status"
+)]
+pub trait Status {
+    async fn get_fpga_state(&self, device_handle: &str) -> Result<String>;
+    async fn get_fpga_flags(&self, device_handle: &str) -> Result<String>;
+    async fn get_overlay_status(&self, platform_str: &str, overlay_handle: &str) -> Result<String>;
+    async fn get_overlays(&self) -> Result<String>;
+    async fn get_platform_type(&self, device_handle: &str) -> Result<String>;
+    async fn get_platform_types(&self) -> Result<String>;
+    async fn get_platform_name(&self, _device_handle: &str) -> Result<String>;
+}
