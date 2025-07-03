5b6b5823cfd88a0e102450e22e81f024a4cd8833
Artie Poole <stuart.poole@canonical.com>
2025-07-03 13:57:04 +0100

    cli: implement status command
    if a device handle is specified, this returns the state of that device formatted to a simple ascii table
    
    otherwise it returns a table of all states of devices and all statuses of overlays
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

---
+++ cli/src/status.rs
@@ -0,0 +1,125 @@
+use crate::proxies::status_proxy;
+use std::collections::HashMap;
+use zbus::Connection;
+
+/// Sends the dbus command to get a list of overlays and parses it
+pub async fn call_get_overlays() -> Result<Vec<String>, zbus::Error> {
+    let connection = Connection::system().await?;
+    let proxy = status_proxy::StatusProxy::new(&connection).await?;
+    let list_str = proxy.get_overlays().await?;
+    let ret_list: Vec<String> = list_str.lines().map(|line| line.to_string()).collect();
+    Ok(ret_list)
+}
+
+/// Sends the dbus command to get the state from an fpga device
+pub async fn call_get_fpga_state(device_handle: &str) -> Result<String, zbus::Error> {
+    let connection = Connection::system().await?;
+    let proxy = status_proxy::StatusProxy::new(&connection).await?;
+    proxy.get_fpga_state(&device_handle).await
+}
+
+/// Sends the dbus command to get the platform_compat_string for a given device
+pub async fn call_get_platform_type(device_handle: &str) -> Result<String, zbus::Error> {
+    let connection = Connection::system().await?;
+    let proxy = status_proxy::StatusProxy::new(&connection).await?;
+    proxy.get_platform_type(device_handle).await
+}
+
+/// Sends the dbus command to get the status string for a given overlay
+async fn call_get_overlay_status(
+    platform: &str,
+    overlay_handle: &str,
+) -> Result<String, zbus::Error> {
+    let connection = Connection::system().await?;
+    let proxy = status_proxy::StatusProxy::new(&connection).await?;
+    proxy.get_overlay_status(platform, overlay_handle).await
+}
+
+/// parses the string from `get_platform_types` interface into a HashMap of
+/// device: platform_compat_string
+pub async fn call_get_platform_types() -> Result<HashMap<String, String>, zbus::Error> {
+    let connection = Connection::system().await?;
+    let proxy = status_proxy::StatusProxy::new(&connection).await?;
+    let ret_str = proxy.get_platform_types().await?;
+    let ret_map = ret_str
+        .lines() // split by '\n'
+        .filter_map(|line| {
+            let mut parts = line.splitn(2, ':');
+            match (parts.next(), parts.next()) {
+                (Some(key), Some(value)) => Some((key.to_string(), value.to_string())),
+                _ => None, // ignore lines without a colon
+            }
+        })
+        .collect();
+    Ok(ret_map)
+}
+
+/// gets the first platform in the container from `call_get_platform_types`
+pub async fn get_first_platform() -> Result<String, zbus::Error> {
+    let platforms = call_get_platform_types().await?;
+    Ok(platforms
+        .values()
+        .next()
+        .unwrap_or(&"universal".to_string())
+        .clone())
+}
+
+/// gets the first overlay in the Vec from `call_get_overlays`
+pub async fn get_first_overlay() -> Result<String, zbus::Error> {
+    let overlays = call_get_overlays().await?;
+    let first = overlays.first().ok_or(zbus::Error::Failure(
+        "Could not find an overlay to remove".to_string(),
+    ))?;
+    Ok(first.clone())
+}
+
+/// gets the first platform in the container from `call_get_platform_types`
+pub async fn get_first_device_handle() -> Result<String, zbus::Error> {
+    let platform = match call_get_platform_types().await?.keys().next() {
+        Some(p) => p.clone(),
+        None => return Err(zbus::Error::Failure("Got no platforms.".to_string())),
+    };
+
+    Ok(platform)
+}
+
+/// gets one fpga state and returns an ascii table as String
+async fn get_fpga_state_message(device_handle: &str) -> Result<String, zbus::Error> {
+    let state = call_get_fpga_state(device_handle).await?;
+    let platform = call_get_platform_type(&device_handle).await?;
+    Ok(format!(
+        "---- DEVICE  ----\n\
+        | dev | platform | state |\n\
+        {device_handle} | {platform} | {state}"
+    ))
+}
+
+/// get all fpga states, gets all overlay statuses, returns a ascii table as String
+async fn get_full_status_message() -> Result<String, zbus::Error> {
+    let mut ret_string = String::from(
+        "---- DEVICES ----\n\
+    | dev | platform | state |\n",
+    );
+
+    for (dev, platform) in call_get_platform_types().await? {
+        let state = call_get_fpga_state(&dev).await?;
+        ret_string += format!("| {dev} | {platform} | {state} |\n").as_str();
+    }
+    ret_string += "\n---- OVERLAYS ----\n\
+                   | overlay | status |\n";
+    for overlay in call_get_overlays().await? {
+        let p = "";
+        let status = call_get_overlay_status(p.into(), &overlay).await?;
+        ret_string.push_str(format!("| {overlay} | {status} |\n").as_ref());
+    }
+    Ok(ret_string)
+}
+
+/// Argument parser for the status command
+pub async fn status_handler(device_handle: &Option<String>) -> Result<String, zbus::Error> {
+    let ret_string = match device_handle {
+        None => get_full_status_message().await?,
+        Some(dev) => get_fpga_state_message(dev.as_str()).await?,
+    };
+    Ok(ret_string)
+}
