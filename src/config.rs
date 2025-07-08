847d5ed760e5b263e94f0e9512946514a64819e0
Artie Poole <stuart.poole@canonical.com>
2025-07-08 13:39:31 +0100

    platforms: refactor path variables
    FW_PREFIX->FIRMWARE_SOURCE_DIR
    SYSFS_PREFIX->FPGA_MANAGERS_DIR
    CONFIGFS_PREFIX->OVERLAY_CONTROL_DIR
    
    added
    + FIRMWARE_LOC_CONTROL_PATH
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,16 @@
-// TODO: this should be provided by the config
-pub static FW_PREFIX: &str = "/lib/firmware/";
-pub static SYSFS_PREFIX: &str = "/sys/class/fpga_manager/";
-pub static CONFIGFS_PREFIX: &str = "/sys/kernel/config/device-tree/overlays/";
+// This file is part of fpgad, an application to manage FPGA subsystem together with device-tree and kernel modules.
+//
+// Copyright 2025 Canonical Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// fpgad is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3, as published by the Free Software Foundation.
+//
+// fpgad is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranties of MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+
+pub static FIRMWARE_SOURCE_DIR: &str = "/lib/firmware/";
+pub static FPGA_MANAGERS_DIR: &str = "/sys/class/fpga_manager/";
+pub static OVERLAY_CONTROL_DIR: &str = "/sys/kernel/config/device-tree/overlays/";
+pub static FIRMWARE_LOC_CONTROL_PATH: &str = "/sys/module/firmware_class/parameters/path";
