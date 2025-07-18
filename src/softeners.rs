774505062f19a0d9e45e3f73121cbc9280815d67
Talha Can Havadar <talha.can.havadar@canonical.com>
2025-07-18 17:25:12 +0200

    softeners: use rust features to make softeners optional (#69)
    This allows users to compile fpgad without softeners or only with
    softeners that they need.
    
    Signed-off-by: Talha Can Havadar <talha.can.havadar@canonical.com>

--- src/softeners.rs
+++ src/softeners.rs
@@ -10,4 +10,6 @@
 //
 // You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
 pub mod error;
+
+#[cfg(feature = "xilinx-dfx-mgr")]
 pub mod xilinx_dfx_mgr;
