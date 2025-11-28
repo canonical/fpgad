4fcaa46f6aba516506a1277ece24b2a587fcd465
Artie Poole <stuart.poole@canonical.com>
2025-11-28 17:50:29 +0000

    First attempt at testing "bitstream_load" only
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

---
+++ .github/testflinger-assets/test_snap_device_script.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -x
+
+#echo "INFO: Installing python dependencies"
+#sudo apt update
+#sudo apt install python3-
+
+# TODO: move the test_data files around?
+
+echo "INFO: Installing fpgad.snap"
+sudo snap install ./fgpad.snap --dangeorus
+
+echo "INFO: Installing provider snap(s)"
+# TODO: detection logic per device? - spread?
+echo "INFO: Installing k26-default-bitstreams snap"
+sudo snap install k26-default-bitstreams --edge # TODO: change track?
+
+
+echo "INFO: Making necessary connections"
+echo "INFO: connecting to fpgad interface"
+sudo snap connect fpgad:fpga
+echo "INFO: connecting to hardware-observe interface"
+sudo snap connect fpgad:hardware-observe
+echo "INFO: connecting to kernel-firmware-control interface"
+sudo snap connect fpgad:kernel-firmware-control
+echo "INFO: connecting to device-tree-overlays interface"
+sudo snap connect fpgad:device-tree-overlays
+echo "INFO: connecting dbus interfaces"
+sudo snap connect fpgad:cli-dbus fpgad:daemon-dbus
+# sudo snap connect fpgad:kernel-module-control # in future
+echo "INFO: Done making necessary connections"
+
+echo "INFO: Running snap test script"
+# NOTE: tarball contains "k24-starter-kits/..." and "k26-starter-kits/..." at tarball root from daemon/tests/test_data
+mkdir -p fpgad/artifacts
+tar -xzvf test_data.gz -C fpgad
+./tests/snap_tests.py 2>&1 | tee fpgad/artifacts/snap_test.log
