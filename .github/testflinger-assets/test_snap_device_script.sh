1a54ac63f4e4c3baa149ed8b0aa751245fe333bc
Artie Poole <stuart.poole@canonical.com>
2025-12-01 14:21:26 +0000

    avoid bad flags related errors
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

--- .github/testflinger-assets/test_snap_device_script.sh
+++ .github/testflinger-assets/test_snap_device_script.sh
@@ -5,34 +5,40 @@ set -euxo pipefail
 #sudo apt install python3-
 
 # TODO: move the test_data files around?
-
-echo "INFO: Removing fpgad that was installed from snap store"
+echo "INFO: Preparing device"
+echo "- Removing fpgad that was installed from snap store"
 sudo snap remove fpgad --purge
+echo "- Removing dfx-mgr"
+sudo apt remove dfx-mgr || true
+echo "- Resetting flags to 0"
+echo 0 | sudo tee /sys/class/fpga_manager/fpga0/flags
 
+echo "- Ensuring snapd is not too busy"
 # Otherwise attempting to make connections will error due to, e.g., `error: snap "snapd" has "auto-refresh" change in progress`
 while sudo snap changes | grep -E "auto-refresh.*(Doing|Undoing|Pending|Hold)" >/dev/null; do
     echo "Snap auto-refresh in progress or queued... waiting 5 more seconds..."
     sleep 5
 done
 
-echo "INFO: Installing fpgad.snap"
+echo "- Installing fpgad.snap"
 sudo snap install ./fpgad.snap --dangerous
 
-echo "INFO: Installing provider snap(s)"
+echo "- Installing provider snap(s)"
 # TODO: detection logic per device? - spread?
-echo "INFO: Installing k26-default-bitstreams snap"
+echo "- Installing k26-default-bitstreams snap"
 sudo snap install k26-default-bitstreams --edge # TODO: change track?
+echo "INFO: Done preparing device"
 
 echo "INFO: Making necessary connections"
-echo "INFO: connecting to fpgad interface"
+echo "- connecting to fpgad interface"
 sudo snap connect fpgad:fpga
-echo "INFO: connecting to hardware-observe interface"
+echo "- connecting to hardware-observe interface"
 sudo snap connect fpgad:hardware-observe
-echo "INFO: connecting to kernel-firmware-control interface"
+echo "- connecting to kernel-firmware-control interface"
 sudo snap connect fpgad:kernel-firmware-control
-echo "INFO: connecting to device-tree-overlays interface"
+echo "- connecting to device-tree-overlays interface"
 sudo snap connect fpgad:device-tree-overlays
-echo "INFO: connecting dbus interfaces"
+echo "- connecting dbus interfaces"
 sudo snap connect fpgad:cli-dbus fpgad:daemon-dbus
 # sudo snap connect fpgad:kernel-module-control # in future
 echo "INFO: Done making necessary connections"
@@ -42,3 +48,4 @@ echo "INFO: Running snap test script"
 mkdir -p fpgad/artifacts
 tar -xzvf test_data.gz -C fpgad
 sudo python3 -m unittest ./snap_tests.py 2>&1 | tee fpgad/artifacts/snap_test.log
+echo "INFO: Done running snap test script"
