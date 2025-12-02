fd761b66f39ec1f749ecd78cf4ca22507659dc18
Artie Poole <stuart.poole@canonical.com>
2025-12-02 12:32:17 +0000

    testing using the debug state issue
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

--- .github/testflinger-assets/test_snap_device_script.sh
+++ .github/testflinger-assets/test_snap_device_script.sh
@@ -6,44 +6,36 @@ set -euxo pipefail
 
 # TODO: move the test_data files around?
 echo "INFO: Preparing device"
-echo "- Removing fpgad that was installed from snap store"
+echo "    --- Removing fpgad that was installed from snap store"
 sudo snap remove fpgad --purge
-echo "- Removing dfx-mgr"
+echo "    --- Removing dfx-mgr"
 sudo apt remove -y dfx-mgr || true
-
-echo "- Ensuring snapd is not too busy"
+echo "    --- Ensuring snapd is not too busy"
 # Otherwise attempting to make connections will error due to, e.g., `error: snap "snapd" has "auto-refresh" change in progress`
-while sudo snap changes | grep -E "auto-refresh.*(Doing|Undoing|Pending|Hold)" >/dev/null; do
-    echo "Snap auto-refresh in progress or queued... waiting 5 more seconds..."
-    sleep 5
+while sudo snap debug state /var/lib/snapd/state.json | grep -qE 'Doing|Undoing|Waiting'; do
+    echo "    --- snapd internal tasks still running... waiting..."
+    sleep 10
 done
-echo "- Disabling auto-refresh for 24 hours"
+echo "    --- Disabling auto-refresh for 24 hours"
 sudo snap refresh --hold=24h
-
-sudo snap debug state /var/lib/snapd/state.json
-
-echo "- Installing fpgad.snap"
+echo "    --- Installing fpgad.snap"
 sudo snap install ./fpgad.snap --dangerous
-sudo snap debug state /var/lib/snapd/state.json
-
-echo "- Installing provider snap(s)"
+echo "    --- Installing provider snap(s)"
 # TODO: detection logic per device? - spread?
-echo "- Installing k26-default-bitstreams snap"
+echo "    --- Installing k26-default-bitstreams snap"
 sudo snap install k26-default-bitstreams --edge # TODO: change track?
 echo "INFO: Done preparing device"
-sudo snap debug state /var/lib/snapd/state.json
 
 echo "INFO: Making necessary connections"
-echo "- connecting to fpgad interface"
+echo "    --- connecting to fpgad interface"
 sudo snap connect fpgad:fpga
-sudo snap debug state /var/lib/snapd/state.json
-echo "- connecting to hardware-observe interface"
+echo "    --- connecting to hardware-observe interface"
 sudo snap connect fpgad:hardware-observe
-echo "- connecting to kernel-firmware-control interface"
+echo "    --- connecting to kernel-firmware-control interface"
 sudo snap connect fpgad:kernel-firmware-control
-echo "- connecting to device-tree-overlays interface"
+echo "    --- connecting to device-tree-overlays interface"
 sudo snap connect fpgad:device-tree-overlays
-echo "- connecting dbus interfaces"
+echo "    --- connecting dbus interfaces"
 sudo snap connect fpgad:cli-dbus fpgad:daemon-dbus
 # sudo snap connect fpgad:kernel-module-control # in future
 echo "INFO: Done making necessary connections"
