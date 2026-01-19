697151e77e560eccedf25a49e08e2c61694aa656
Artie Poole <stuart.poole@canonical.com>
2026-01-19 18:10:14 +0000

    docs: document the dbus module
    documentation added for:
    - helper functions
    - StatusInterface
    - ControlInterface
    
    Documentation added at
    - top level
    - class level
    - function level
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

--- daemon/src/comm/dbus.rs
+++ daemon/src/comm/dbus.rs
@@ -10,6 +10,48 @@
 //
 // You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
 
+//!
+//! # FPGAd dbus interfaces
+//!
+//! There are two interfaces available: `status`, which contains methods to access read-only properties of the FPGA and overlay subsystems, and `control` which contains methods to access writable properties of the FPGA and overlay subsystems.
+//! Directly following this paragraph is a summary of all available methods, with the rest of this document explaining each method in detail.
+//! Please note that these interfaces are used in the `cli` part of FPGAd (e.g. in [load.rs](../../../../cli/src/load.rs)), and so those implementations can be used as examples with greater context than those provided here.
+//! There is further usage documentation, including the use of these interfaces from cmdline by way of `dbusctl`, in [daemon/README.md](../../../README.md).
+//!
+//!
+//! For both interfaces, all methods are asynchronous and should be awaited immediately to avoid async pollution.
+//!
+//! ## Custom Errors
+//!
+//! All functions are shown to return the `fdo::Error` type, which is a wrapper for "FreeDesktopOrg", i.e. these are DBus error types. In the case that FPGAd returns an error, and it is not a DBus communication error, this error will be of type `fdo::Error:Failure` and the error string with begin with the `FpgadError` identifier such as `"FpgadError::Argument:"` i.e. the full error will be reported (from `busctl`) as
+//! ```text
+//! Call failed: FpgadError::Argument: <error text>
+//! ```
+//! or in the case of an IOError, the rust error will appear like:
+//! ```text
+//! [2026-01-08T16:29:05Z ERROR cli] org.freedesktop.DBus.Error.IOError: FpgadError::IOWrite: <fpgad's error text>: No such file or directory (os error 2)
+//! Error: MethodError(OwnedErrorName("org.freedesktop.DBus.Error.IOError"), Some("FpgadError::IOWrite: <fpgad's error text>: No such file or directory (os error 2)"), Msg { type: Error, serial: 11, sender: UniqueName(":1.77"), reply-serial: 4, body: Str, fds: [] })
+//! ```
+//! whereby the `FPGAdError` type could  be mapped to an equivalent `fdo::Error` type.
+//!
+//! ## FPGA API Summary
+//!
+//! | Interface | Method                   | Parameters                                                                                                     | Returns / Notes                                                                                                                             |
+//! |-----------|--------------------------|----------------------------------------------------------------------------------------------------------------|---------------------------------------------------------------------------------------------------------------------------------------------|
+//! | [status](status_interface::StatusInterface)   | [`get_fpga_state`](status_interface::StatusInterface::get_fpga_state)           | `platform_string: &str`, `device_handle: &str`                                             | `String` – Current FPGA state (`unknown`, `operating`, etc.)                                                                                |
+//! | [status](status_interface::StatusInterface)   | [`get_fpga_flags`](status_interface::StatusInterface::get_fpga_flags)           | `platform_string: &str`, `device_handle: &str`                                             | `String` – Current FPGA flags from hexadecimal integer to string                                                                            |
+//! | [status](status_interface::StatusInterface)   | [`get_overlay_status`](status_interface::StatusInterface::get_overlay_status)   | `platform_string: &str`, `overlay_handle: &str`                                            | `String` – Overlay status; errors if handle empty or invalid                                                                                |
+//! | [status](status_interface::StatusInterface)   | [`get_overlays`](status_interface::StatusInterface::get_overlays)               | None                                                                                       | `String` – List of available overlay handles (`\n` separated)                                                                               |
+//! | [status](status_interface::StatusInterface)   | [`get_platform_type`](status_interface::StatusInterface::get_platform_type)     | `device_handle: &str`                                                                      | `String` – Compatibility string for device                                                                                                  |
+//! | [status](status_interface::StatusInterface)   | [`get_platform_types`](status_interface::StatusInterface::get_platform_types)   | None                                                                                       | `String` – List of all fpga devices and their compatibility strings (`<device>:<compat>\n`)                                                 |
+//! | [status](status_interface::StatusInterface)   | [`read_property`](status_interface::StatusInterface::read_property)             | `property_path_str: &str`                                                                  | `String` – Contents of arbitrary FPGA attribute value                                                                                       |
+//! | [control](control_interface::ControlInterface) | [`set_fpga_flags`](control_interface::ControlInterface::set_fpga_flags)         | `platform_string: &str`, `device_handle: &str`, `flags: u32`                               | `String` – Confirmation with new flags in hex                                                                                               |
+//! | [control](control_interface::ControlInterface) | [`write_bitstream_direct`](control_interface::ControlInterface::write_bitstream_direct) | `platform_string: &str`, `device_handle: &str`, `bitstream_path_str: &str`, `firmware_lookup_path: &str` | `String` – Confirmation of bitstream load; acquires write lock                                                                              |
+//! | [control](control_interface::ControlInterface) | [`apply_overlay`](control_interface::ControlInterface::apply_overlay)           | `platform_string: &str`, `overlay_handle: &str`, `overlay_source_path: &str`, `firmware_lookup_path: &str` | `String` – Overlay applied; confirmation including firmware prefix; write lock used to protect against firmware search path race conditions |
+//! | [control](control_interface::ControlInterface) | [`remove_overlay`](control_interface::ControlInterface::remove_overlay)         | `platform_string: &str`, `overlay_handle: &str`                                            | `String` – Overlay removed; confirmation including overlay filesystem path                                                                  |
+//! | [control](control_interface::ControlInterface) | [`write_property`](control_interface::ControlInterface::write_property)         | `property_path_str: &str`, `data: &str`                                                    | `String` – Confirmation of data written; path must be under `/sys/class/fpga_manager/`                                                      |
+//! | [control](control_interface::ControlInterface) | [`write_property_bytes`](control_interface::ControlInterface::write_property_bytes) | `property_path_str: &str`, `data: &[u8]`                                                   | `String` – Confirmation of bytes written; path must be under `/sys/class/fpga_manager/`                                                     |
+
 pub mod control_interface;
 pub mod status_interface;
 
@@ -19,6 +61,23 @@ use crate::system_io::{fs_read, fs_write};
 use log::trace;
 use std::path::{Component, Path, PathBuf};
 
+/// Read the current contents of an FPGA device property, e.g. "name". The property path must be a subdirectory of the fpga manager directory (typically, /sys/class/fpga_manager/)
+///
+/// # Arguments
+///
+/// * `property_path_str`: path to the variable to read e.g. /sys/class/fpga_manager/fpga0/name
+///
+/// # Returns: `Result<String, FpgadError>`
+/// * `String` - the contents of the property path
+///
+/// * `FpgadError::Argument` if the path is not found within the compile time [config::FPGA_MANAGERS_DIR]
+///
+/// # Examples
+///
+/// ```
+/// let device_name = fs_read_property("/sys/class/fpga_manager/fpga0/name")?;
+/// assert_eq!(device_name, "Xilinx ZynqMP FPGA Manager\n")
+/// ```
 pub fn fs_read_property(property_path_str: &str) -> Result<String, FpgadError> {
     let property_path = Path::new(property_path_str);
     if !property_path.starts_with(Path::new(config::FPGA_MANAGERS_DIR)) {
@@ -32,6 +91,20 @@ pub fn fs_read_property(property_path_str: &str) -> Result<String, FpgadError> {
 }
 
 #[allow(dead_code)]
+/// Read the currently specified firmware search path.
+/// See [these kernel docs](https://docs.kernel.org/driver-api/firmware/fw_search_path.html)
+/// for more information on the process.
+///
+/// # Returns: `Result<String, FpgadError>`
+/// * `String` - The contents of the firmware search path variable.
+/// * `FpgadError::IOWRite` (or similar IO error) if writing fails for any reason.
+///
+/// # Examples
+///
+/// ```rust
+/// let search_path_str = read_firmware_source_dir()?;
+/// assert_eq!(search_path_str, "/lib/firmware/my_firmware_dir");
+/// ```
 pub fn read_firmware_source_dir() -> Result<String, FpgadError> {
     trace!(
         "Reading fw prefix from {}",
@@ -41,6 +114,23 @@ pub fn read_firmware_source_dir() -> Result<String, FpgadError> {
     fs_read(fw_lookup_override)
 }
 
+/// Write a specified path to the systems firmware search path.
+/// See [these kernel docs](https://docs.kernel.org/driver-api/firmware/fw_search_path.html)
+/// for more information on the process.
+///
+/// # Arguments
+///
+/// * `new_path`: path inside which firmware can be found
+///
+/// # Returns: `Result<(), FpgadError>`
+/// * `()` on success
+/// * `FpgadError::IOWRite` (or similar IO error) if writing fails for any reason.
+///
+/// # Examples
+///
+/// ```
+/// assert!(write_firmware_source_dir("/lib/firmware/my_firmware_dir").is_ok());
+/// ```
 pub fn write_firmware_source_dir(new_path: &str) -> Result<(), FpgadError> {
     trace!(
         "Writing fw prefix {} to {}",
@@ -51,6 +141,22 @@ pub fn write_firmware_source_dir(new_path: &str) -> Result<(), FpgadError> {
     fs_write(fw_lookup_override, false, new_path)
 }
 
+/// Splits a Path object into its parent directory and basename/filename
+///
+/// # Arguments
+///
+/// * `path`: path to be split
+///
+/// # Returns: `Result<(PathBuf, PathBuf), FpgadError>`
+/// * `(PathBuf, PathBuf)` - Tuple of parent directory and basename/filename
+/// * `FpgadError::Argument` on invalid `path` or `path` is a root directory (no parent)
+/// # Examples
+///
+/// ```rust
+/// let (parent, base) = extract_path_and_filename(Path::new("/lib/firmware/file.bin"));
+/// assert_eq!(parent.to_string_lossy(), "/lib/firmware");
+/// assert_eq!(base.to_string_lossy(), "file.bin");
+/// ```
 pub fn extract_path_and_filename(path: &Path) -> Result<(PathBuf, PathBuf), FpgadError> {
     // Extract filename
     let filename = path
@@ -72,6 +178,22 @@ pub fn extract_path_and_filename(path: &Path) -> Result<(PathBuf, PathBuf), Fpga
 }
 
 /// Helper function to check that a device with given handle does exist.
+///
+/// # Arguments
+///
+/// * `device_handle`: name of the device in sysfs, e.g. `fpga0`
+///
+/// # Returns: `Result<(), FpgadError>`
+/// * `()` on success
+/// * `FpgadError::Argument` in the case that `device_handle` is not found on the system or
+///   device_handle contains invalid characters/is empty.
+///
+/// # Examples
+///
+/// ```
+/// assert!(validate_device_handle("fpga0").is_ok())
+/// assert!(validate_device_handle("").is_err())
+/// ```
 pub(crate) fn validate_device_handle(device_handle: &str) -> Result<(), FpgadError> {
     if device_handle.is_empty() || !device_handle.is_ascii() {
         return Err(FpgadError::Argument(format!(
@@ -102,22 +224,27 @@ pub(crate) fn validate_device_handle(device_handle: &str) -> Result<(), FpgadErr
 /// * `source_path`: the full path to the target file (or containing directory?)
 /// * `firmware_path`: the root common path for all files to be loaded by the FW subsystem
 ///
-/// returns: Result<(PathBuf, PathBuf), FpgadError> A tuple of (prefix, suffix) where prefix is
-/// typically used as the fw_lookup_path and the suffix is remaining relative path from that prefix
-///
+/// # Returns: `Result<(PathBuf, PathBuf), FpgadError>`
+/// * `(PathBuf, PathBuf)` - A tuple of (prefix, suffix) where prefix is
+///   typically used as the fw_lookup_path and the suffix is remaining relative path from that prefix
+/// * `FpgadError::Argument` in case `firmware_path` is not within `source_path`, or for inputs
+///   resulting in an empty suffix value
 /// # Examples
 ///
 /// ```
-/// use std::path::Path;
+/// #use std::path::Path;
 /// let (prefix, suffix) = make_firmware_pair(
 ///      Path::new("/lib/firmware/file.bin"),
 ///      Path::new("/lib/firmware/"),
-/// )?; // returns prefix = "/lib/firmware/", suffix = "file.bin"
+/// )?;
+/// assert_eq!(prefix, "/lib/firmware");
+/// assert_eq!(suffix, "file.bin");
 /// ```
 pub(crate) fn make_firmware_pair(
     source_path: &Path,
     firmware_path: &Path,
 ) -> Result<(PathBuf, PathBuf), FpgadError> {
+    // No firmware search path provided, so just try to use parent dir
     if firmware_path.as_os_str().is_empty() {
         return extract_path_and_filename(source_path);
     }
--- daemon/src/comm/dbus/status_interface.rs
+++ daemon/src/comm/dbus/status_interface.rs
@@ -10,6 +10,11 @@
 //
 // You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
 
+//!
+//! The `StatusInterface` provides asynchronous methods to query the state, flags, and overlays of FPGA devices on the system.
+//! All methods return a `Result<String, fdo::Error>` and are designed for DBus usage.
+//! If FPGAd raises the error, then the `fdo::Error` strings are prepended with the relevant FPGAd error type e.g. `FpgadError::Argument: <error text>`. See [crate::comm::dbus] for a summary of this interface's methods.
+//!
 use crate::config;
 use crate::platforms::platform::{list_fpga_managers, read_compatible_string};
 use crate::platforms::platform::{platform_for_known_platform, platform_from_compat_or_device};
@@ -20,10 +25,32 @@ use crate::system_io::fs_read_dir;
 use log::{error, info};
 use zbus::{fdo, interface};
 
+/// Instance of a [zbus::object_server::Interface] constructed using the [zbus::interface] macro.
 pub struct StatusInterface {}
 
+/// See [crate::comm::dbus] for a summary of this interface's methods, or
+/// [crate::comm::dbus::status_interface] for a summary of this interface in general.
 #[interface(name = "com.canonical.fpgad.status")]
 impl StatusInterface {
+    /// The device handle (e.g., `fpga0`) of the FPGA.
+    ///
+    /// # Arguments
+    ///
+    /// * `platform_string`: Platform compatibility string.
+    /// * `device_handle`: The device handle (e.g., `fpga0`) of the FPGA.
+    ///
+    /// # Returns: `Result<String, fdo::Error>`
+    /// * `Ok(String)` – the current FPGA state (e.g., `"operating"`, `"unknown"`).
+    /// * `Err(fdo::Error)` if the device handle is invalid or the platform cannot be determined.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use crate::comm::dbus::StatusInterface
+    /// # let status_interface = StatusInterface()
+    /// let state = status_interface.get_fpga_state("xlnx,zynqmp-pcap-fpga", "fpga0").await?;
+    /// assert_eq!(state, "operating");
+    /// ```
     async fn get_fpga_state(
         &self,
         platform_string: &str,
@@ -35,6 +62,27 @@ impl StatusInterface {
         Ok(platform.fpga(device_handle)?.state()?)
     }
 
+    /// Retrieve the current flags set for a specified FPGA device as a hexadecimal ascii string
+    /// (missing `0x` prefix).
+    ///
+    /// # Arguments
+    ///
+    /// * `platform_string`: Platform compatibility string.
+    /// * `device_handle`: The device handle (e.g., `fpga0`) of the FPGA.
+    ///
+    /// # Returns: `Result<String, fdo::Error>`
+    /// * `Ok(String)` – hexadecimal representation of flags without `0x` hex prefix (e.g. "20"
+    /// for decimal value of 32)
+    /// * `Err(fdo::Error)` on invalid handle or platform error.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use crate::comm::dbus::StatusInterface
+    /// # let status_interface = StatusInterface()
+    /// let flags = status_interface.get_fpga_flags("xlnx,zynqmp-pcap-fpga", "fpga0").await?;
+    /// assert_eq!(flags, "20");
+    /// ```
     async fn get_fpga_flags(
         &self,
         platform_string: &str,
@@ -49,7 +97,29 @@ impl StatusInterface {
             .map(|flags| flags.to_string())?)
     }
 
-    async fn get_overlay_status(
+    /// Retrieve the status of a specific device-tree overlay.
+    ///
+    /// # Arguments
+    ///
+    /// * `platform_string`: Platform compatibility string.
+    /// * `overlay_handle`: The handle (name) of the device-tree overlay to query.
+    ///
+    /// # Returns: `Result<String, fdo::Error>`
+    /// * `Ok(String)` – Status of the overlay (e.g., `"loaded"`, `"unapplied"`).
+    /// * `Err(fdo::Error)`  If the overlay handle is empty or not found.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use crate::comm::dbus::StatusInterface
+    /// # let status_interface = StatusInterface()
+    /// let status = status_interface.get_overlay_status(
+    ///     "xlnx,zynqmp-pcap-fpga",
+    ///     "my_overlay"
+    /// ).await?;
+    /// assert_eq!(status, "applied");
+    /// ```
+    pub async fn get_overlay_status(
         &self,
         platform_string: &str,
         overlay_handle: &str,
@@ -69,12 +139,68 @@ impl StatusInterface {
             .status()?)
     }
 
+    /// Retrieve a list of all available device-tree overlay handles (names of the device-tree
+    /// overlay directories present on the system)
+    ///
+    /// # Returns: `Result<String, fdo::Error>`
+    /// * `Ok(String)` – List of existing overlay handles, separated by newline (`\n`).
+    /// * `Err(fdo::Error)` If the overlay handle is empty or not found.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use crate::comm::dbus::StatusInterface
+    /// # let status_interface = StatusInterface()
+    /// let overlays = status_interface.get_overlays().await?;
+    /// ```
+    ///
+    /// # Example Ok(String) values
+    /// ## no overlays present:
+    /// ```rust
+    /// assert_eq!(overlays, "");
+    /// ```
+    ///
+    /// ## One overlay present:
+    /// ```text
+    /// assert_eq!(overlays, "my_overlay");
+    /// ```
+    /// ## Multiple overlays present:
+    /// ```rust
+    /// assert_eq!(overlays, "my_overlay1\nmy_overlay2\n...\nmy_overlayN");
+    /// ```
+    ///
     async fn get_overlays(&self) -> Result<String, fdo::Error> {
         info!("get_overlays called");
         let overlay_handles = fs_read_dir(config::OVERLAY_CONTROL_DIR.as_ref())?;
         Ok(overlay_handles.join("\n"))
     }
 
+    /// Get the compatibility string for a single FPGA device.
+    ///
+    /// # Arguments
+    ///
+    /// * `device_handle`: FPGA device handle.
+    ///
+    /// # Returns: `Result<String, Error>`
+    /// * `Ok(String)` – Device's platform compatibility string.
+    /// * `Err(fdo::Error)` if device validation or reading the compatible string fails.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use crate::comm::dbus::StatusInterface
+    /// # let status_interface = StatusInterface()
+    /// let platform_string = status_interface.get_platform_type("").await?;
+    /// assert_eq!(platform_string, "xlnx,zynqmp-pcap-fpga");
+    /// ```
+    ///
+    /// ```rust
+    /// # use crate::comm::dbus::StatusInterface
+    /// # let status_interface = StatusInterface()
+    /// let platform_string = status_interface.get_platform_type("fpga0").await?;
+    /// assert_eq!(platform_string, "xlnx,zynqmp-pcap-fpga");
+    /// ```
+    ///
     async fn get_platform_type(&self, device_handle: &str) -> Result<String, fdo::Error> {
         info!("get_platform_type called with device_handle: {device_handle}");
         validate_device_handle(device_handle)?;
@@ -82,6 +208,34 @@ impl StatusInterface {
         Ok(ret_string.to_string())
     }
 
+    /// Get a list of all FPGA devices and their platform types.
+    ///
+    /// # Arguments
+    ///
+    /// * `device_handle`: FPGA device handle.
+    ///
+    /// # Returns: `Result<String, Error>`
+    /// * `Ok(String)` – Each line formatted as `<device_handle>:<platform_string>\n`.
+    ///    Devices without a valid string appear as
+    ///   `<device_handle>:\n`.
+    /// * `Err(fdo::Error)` if device validation or reading the compatible string fails.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use crate::comm::dbus::StatusInterface
+    /// # let status_interface = StatusInterface()
+    /// let platform_map = status_interface.get_platform_types().await?;
+    /// ```
+    /// If only one device:
+    /// ```
+    /// assert_eq!(platform_map, "fpga0:xlnx,zynqmp-pcap-fpga\n");
+    /// ```
+    /// or for multiple devices
+    /// ```rust
+    /// assert_eq!(platform_map, "fpga0:xlnx,zynqmp-pcap-fpga\nfpga1:xlnx,versal-fpga\n");
+    /// ````
+    ///
     async fn get_platform_types(&self) -> Result<String, fdo::Error> {
         info!("get_platform_types called");
         let mut ret_string = String::new();
@@ -97,7 +251,25 @@ impl StatusInterface {
         Ok(ret_string)
     }
 
-    /// use to read a device property from /sys/class/fpga_manager/<device>/** that does not have a specific interface
+    /// Read an arbitrary FPGA device property from `/sys/class/fpga_manager/<device>/`.
+    ///
+    /// # Arguments
+    ///
+    /// * `property_path_str`: Full path to the property file.
+    ///
+    /// # Returns: `Result<String, Error>`
+    /// * `Ok(String)` – Contents of the property file.
+    /// * `Err(fdo::Error)` If the property cannot be read.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// let name = status_interface.
+    ///     read_property("/sys/class/fpga_manager/fpga0/name")
+    ///     .await?;
+    /// assert_eq!(name, "Xilinx ZynqMP FPGA Manager\n");
+    /// ```
+    ///
     async fn read_property(&self, property_path_str: &str) -> Result<String, fdo::Error> {
         info!("read_property called with property_path_str: {property_path_str}");
         Ok(fs_read_property(property_path_str)?)
