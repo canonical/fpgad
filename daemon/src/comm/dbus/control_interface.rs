40b881963befb4890edd30d656cdbbdaef9af4d8
Artie Poole <stuart.poole@canonical.com>
2026-03-16 16:32:11 +0000

    cli: add remove_bitstream command for dfx-mgr testing
    - Add remove_bitstream subcommand to CLI
    - Improve display formatting with pretty-print macros
    - Add corresponding dbus method
    
    Enables testing of dfx-mgr bitstream removal functionality.
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

--- daemon/src/comm/dbus/control_interface.rs
+++ daemon/src/comm/dbus/control_interface.rs
@@ -109,6 +109,7 @@ impl ControlInterface {
         device_handle: &str,
         flags: u32,
     ) -> Result<String, fdo::Error> {
+        // TODO(Artie): consider whether this should be a platform specific function or not.
         info!("set_fpga_flags called with name: {device_handle} and flags: {flags}");
         validate_device_handle(device_handle)?;
         let platform = platform_from_compat_or_device(platform_string, device_handle)?;
@@ -172,6 +173,9 @@ impl ControlInterface {
         bitstream_path_str: &str,
         firmware_lookup_path: &str,
     ) -> Result<String, fdo::Error> {
+        // TODO(Artie): refactor this code to be platform specific. The dfx-mgr implementation does
+        //  the firmware source dir write under the hood. This could instead be in a different #
+        //  interface for dfx-mgr, but then the platform detection becomes useless.
         info!("load_firmware called with name: {device_handle} and path_str: {bitstream_path_str}");
         validate_device_handle(device_handle)?;
         let path = Path::new(bitstream_path_str);
@@ -247,6 +251,8 @@ impl ControlInterface {
         overlay_source_path: &str,
         firmware_lookup_path: &str,
     ) -> Result<String, fdo::Error> {
+        // TODO(Artie): refactor this code to be platform specific. The dfx-mgr implementation does
+        //  the firmware source dir write under the hood.
         info!(
             "apply_overlay called with platform_string: {platform_string}, overlay_handle: \
             {overlay_handle} and overlay_path: {overlay_source_path}",
@@ -364,6 +370,7 @@ impl ControlInterface {
         property_path_str: &str,
         data: &str,
     ) -> Result<String, fdo::Error> {
+        // TODO(Artie): consider whether this should be a platform specific function or not.
         info!("write_property called with property_path_str: {property_path_str} and data: {data}");
         let property_path = validate_property_path(Path::new(property_path_str))?;
         fs_write(&property_path, false, data)?;
@@ -403,6 +410,7 @@ impl ControlInterface {
         property_path_str: &str,
         data: &[u8],
     ) -> Result<String, fdo::Error> {
+        // TODO(Artie): consider whether this should be a platform specific function or not.
         info!(
             "write_property called with property_path_str: {property_path_str} and data: {data:?}"
         );
