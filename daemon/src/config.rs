2843da2e2164c13f8b87e502f19327fe884a0ae9
Artie Poole <stuart.poole@canonical.com>
2026-03-24 11:31:35 +0000

    docs: dbus: add DBus API documentation  (#145)
    * docs: document the dbus module
    
    documentation added for:
    - helper functions
    - StatusInterface
    - ControlInterface
    
    Documentation added at
    - top level
    - class level
    - function level
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * docs: CLI add top level docstring
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * docs: fix misinformation in daemon busctl examples
    
    the hex value is returned as undecorated ascii (no 0x prefix)
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * docs: document the static variables in config.rs
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * workflows: use @rev263 for all submit actions
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * cli: docs: address cli main docstring typos
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * docs: dbus: address set_fpga_flags misinformation in docstring
    
    It said device handle doesn't need to be provided.
    This is incorrect - platform string is not required but device handle is.
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * docs: dbus: address missing result assignments in write_bitstream_direct examples
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * docs: dbus: remove misleading StatusInterface creation and add no_run
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * docs: dbus: address status interface docstring mistakes
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    * docs: dbus: address dbus helper docs mistakes
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>
    
    ---------
    
    Signed-off-by: Artie Poole <stuart.poole@canonical.com>

--- daemon/src/config.rs
+++ daemon/src/config.rs
@@ -10,6 +10,15 @@
 //
 // You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
 
+/// The driver-decided location of fpga_manager objects. Typically `/sys/class/fpga_manager/`.
 pub static FPGA_MANAGERS_DIR: &str = "/sys/class/fpga_manager/";
+
+/// The default location of the device-tree overlay directory within the config filesystem. Typically `/sys/kernel/config/device-tree/overlays/`, but often remounted to `/config/device-tree/overlays/`.
 pub static OVERLAY_CONTROL_DIR: &str = "/sys/kernel/config/device-tree/overlays/";
+
+/// The path of the file which can be written to in order to add an additional firmware search path
+/// to the system. Typically `/sys/module/firmware_class/parameters/path`.
+/// See
+/// [https://docs.kernel.org/driver-api/firmware/fw_search_path.html](https://docs.kernel.org/driver-api/firmware/fw_search_path.html)
+/// for more information
 pub static FIRMWARE_LOC_CONTROL_PATH: &str = "/sys/module/firmware_class/parameters/path";
